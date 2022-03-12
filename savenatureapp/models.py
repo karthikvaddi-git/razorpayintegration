79aa700059c8da32c30a4c61962e55d1914fb8d6
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2022-03-12 23:18:41 +0530

    donation form created

--- savenatureapp/models.py
+++ savenatureapp/models.py
@@ -2,7 +2,8 @@ from django.db import models
 
 # Create your models here.
 class savenature(models.Model):
-    phonenumber=models.CharField(max_length=1000)
+    name=models.CharField(max_length=1000)
+    email=models.EmailField(max_length=200)
     amount=models.CharField(max_length=500)
     paymentid=models.CharField(max_length=1000)
     paid=models.BooleanField(default=False)
--- savenatureapp/views.py
+++ savenatureapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from decouple import config
 from .models import savenature
 # Create your views here.
-
+import razorpay
 def index(request):
     print(config('RAZOR_KEY_ID'))
     config('RAZOR_KEY_SECRET')
@@ -11,11 +11,12 @@ def index(request):
 
 def pay(request):
     if request.method=="POST":
+        print(request.POST.get('emailid'))
 
         client = razorpay.Client(auth=(config('RAZOR_KEY_ID'), config('RAZOR_KEY_SECRET')))
-        payment = client.order.create({'amount': 500, 'currency': 'INR', 'payment_capture': '1'})
-        saveobj = savenature(phonenumeber=name, amount=amount, paymentid=payment['id'])
-        saveobj.save()
+       # payment = client.order.create({'amount': 500, 'currency': 'INR', 'payment_capture': '1'})
+      #  saveobj = savenature(phonenumeber=name, amount=amount, paymentid=payment['id'])
+       # saveobj.save()
         return render(request, "pay.html")
     else:
         return render(request,"pay.html")
