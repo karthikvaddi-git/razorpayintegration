f15c2d2e27e97048b028821abd5d716ea862ae1d
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2022-03-14 23:08:25 +0530

    payment signature verification was done

--- savenatureapp/views.py
+++ savenatureapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from decouple import config
 from .models import savenature
+from django.http import HttpResponse
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 import razorpay
@@ -16,19 +17,21 @@ def pay(request):
         name=request.POST.get('nameid')
         email=request.POST.get('emailid')
         amount=request.POST.get('amountid')
+        amount=int(amount)*100
+
 
 
         client = razorpay.Client(auth=(config('RAZOR_KEY_ID'), config('RAZOR_KEY_SECRET')))
         payment = client.order.create({'amount': amount, 'currency': 'INR', 'payment_capture': '1'})
         print(payment)
 
-        print(payment)
+
 
         saveobj = savenature(name=name, email=email, paymentid=payment['id'],amount=amount)
         saveobj.save()
         context={'payment':payment}
 
-        return render(request, "pay.html",{'payment':payment})
+        return render(request, "pay.html",{'payment':payment,'saveobj':saveobj})
 
     else:
         return render(request,"pay.html")
@@ -37,18 +40,43 @@ def pay(request):
 @csrf_exempt
 def success(request):
     if request.method == "POST":
-        a = (request.POST)
-        order_id = ""
-        for key, val in a.items():
-            if key == "razorpay_order_id":
-                order_id = val
-                break
 
-        user = savenature.objects.filter(paymentid=order_id).first()
-        user.paid = True
-        user.save()
 
-    return render(request, "success.html")
+        print(request.POST)
+        orderid=request.POST['razorpay_order_id']
+        paymentid=request.POST['razorpay_payment_id']
+        siganture=request.POST['razorpay_signature']
+
+
+
+        client = razorpay.Client(auth=(config('RAZOR_KEY_ID'), config('RAZOR_KEY_SECRET')))
+        params_dict = {
+            'razorpay_order_id': orderid,
+            'razorpay_payment_id': paymentid ,
+            'razorpay_signature': siganture
+        }
+
+
+
+        try:
+
+                status = client.utility.verify_payment_signature(params_dict)
+                return HttpResponse("payemnet succesful")
+
+
+        except:
+
+
+                # if there is an error while capturing payment.
+                return HttpResponse(" arey paymentfail.html")
+        else:
+
+            return render(request,"success.html")
+
+
+
+
+
 
 
 def rz(request):
