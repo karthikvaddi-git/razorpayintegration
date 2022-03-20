1a1181ab33bbba36aaf7770de37f53bb2b0c1e64
karthikvaddi-git <karthikvaddicodingandgames@gmail.com>
2022-03-20 09:32:28 +0530

    allowed host

--- savenature/settings.py
+++ savenature/settings.py
@@ -26,7 +26,7 @@ RAZOR_KEY_SECRET=config('RAZOR_KEY_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
